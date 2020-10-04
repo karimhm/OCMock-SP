d67a891e430fa309cc94526a708f7eb0fa1a5c6b
Karim <karim@karhm.com>
2020-10-04 14:37:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OCMock",
-            url: "https://github.com/karimhm/OCMock/releases/download/3.7.1/OCMock.xcframework.zip",
+            url: "https://github.com/karimhm/OCMock-SP/releases/download/3.7.1/OCMock.xcframework.zip",
             checksum: "fe1822d2b1d3f93c89cf0ae83c03eefc98c0fc69e8108ad578f554cb0a0eba61"
         )
     ]
